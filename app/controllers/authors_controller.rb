6201ccab542e6bf030ce8c3fe9d592699c32daf1
Jiashun <x19134118@student.ncirl.ie>
2022-12-07 12:05:19 +0000

    style devise views

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,6 @@
 class AuthorsController < ApplicationController
   before_action :set_author, only: %i[ show edit update destroy ]
+  before_action :authenticate_user!, :except => [:index, :show]
 
   # GET /authors or /authors.json
   def index
